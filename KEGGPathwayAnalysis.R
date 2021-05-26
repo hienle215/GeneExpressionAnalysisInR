e479e083520250827e05eea8fd2ace4c8730b63b
Thi Le (TAU) <hien.le@tuni.fi>
2021-05-26 14:10:50 +0300

    KEGG pathway analysis structure

---
+++ KEGGPathwayAnalysis.R
@@ -0,0 +1,12 @@
+###Hands on GSEA-based pathway analysis
+Hands on GSEA-based pathway analysis
+
+Now let's do a GSEA analysis. First, download the GSEA.rnk file (you'll have to unzip it). This file has two columns: one for gene id, and the other for gene rank produced using signal2noise method that was discussed in lecture. Go to http://www.webgestalt.org/ and set the following parameters:
+  
+  - Organism: Homo sapiens
+
+- Method: GSEA
+
+- Functional database: pathway --> KEGG
+
+Then upload the rank file and click submit. Wait for a little and answer the quiz questions.
